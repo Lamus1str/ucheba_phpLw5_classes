b38c25fec6aad413f0c5211ac96a6bd6f008f8a7
Polyzhan <almazovi2000@gmail.com>
2026-05-07 11:14:01 +0300

    Форматирование

--- src/ChocolateFactory.php
+++ src/ChocolateFactory.php
@@ -4,18 +4,18 @@ namespace User\ChocolateFactoryLw5;
 
 class ChocolateFactory
 {
-    public function orderSnack(string $type)
-    {
-				$snack = $this->createSnack($type);
-				$snack->prepare();
-				$snack->cut();
-    }
+	public function orderSnack(string $type)
+	{
+		$snack = $this->createSnack($type);
+		$snack->prepare();
+		$snack->cut();
+	}
 
-    private function createSnack(string $className): Snack
-    {
-			if (is_subclass_of($className, Snack::class)) {
-				return new $className();
-			}
-			return new Snack();
-    }
+	private function createSnack(string $className): Snack
+	{
+		if (is_subclass_of($className, Snack::class)) {
+			return new $className();
+		}
+		return new Snack();
+	}
 }
--- src/Snack.php
+++ src/Snack.php
@@ -4,36 +4,36 @@ namespace User\ChocolateFactoryLw5;
 
 class Snack
 {
-    protected string $name;
-    protected string $chocolate;
-    protected array $topings;
+  protected string $name;
+  protected string $chocolate;
+  protected array $topings;
 
-    public function __construct()
-    {
-        $this->name = "Простая сладость";
-        $this->chocolate = "Молочный шоколад";
-        $this->topings = [];
-    }
+  public function __construct()
+  {
+    $this->name = "Простая сладость";
+    $this->chocolate = "Молочный шоколад";
+    $this->topings = [];
+  }
 
-    public function prepare()
-    {
-        echo "Началось создание сладости ", $this->name, PHP_EOL;
-        echo "Добавляется шоколад ", $this->chocolate, PHP_EOL;
-        echo "Добавляются начинки: ";
-        $first = true;
-        foreach ($this->topings as $toping) {
-            if ($first) {
-                $first = false;
-            } else {
-                echo ", ";
-            }
-            echo $toping;
-        }
-        echo PHP_EOL;
+  public function prepare()
+  {
+    echo "Началось создание сладости ", $this->name, PHP_EOL;
+    echo "Добавляется шоколад ", $this->chocolate, PHP_EOL;
+    echo "Добавляются начинки: ";
+    $first = true;
+    foreach ($this->topings as $toping) {
+      if ($first) {
+        $first = false;
+      } else {
+        echo ", ";
+      }
+      echo $toping;
     }
+    echo PHP_EOL;
+  }
 
-    public function cut()
-    {
-        echo "Не требуется нарезка", PHP_EOL;
-    }
+  public function cut()
+  {
+    echo "Не требуется нарезка", PHP_EOL;
+  }
 }
